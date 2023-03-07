016475e7ff3e3031213b2f6dae3b842d7e5cdf05
phonesavanhDEV <phonesavanh6912@gmail.com>
2023-03-07 06:32:15 +0000

    Commit message

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -117,7 +117,7 @@ async function getAllUsers(req, res) {
 
     } catch (err) {
       console.error(err);
-      res.status(500).json({ error: 'Server error' });
+      // res.status(500).json({ error: 'Server error' });
     }
   }
 
