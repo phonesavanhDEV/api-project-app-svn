b33466acaeb6b32434966774a4603ad86fba7bdd
phonesavanhDEV <80586029+phonesavanhDEV@users.noreply.github.com>
2023-03-07 11:59:52 +0700

    script

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,30 @@
+// const loginForm = document.getElementById('login-form');
+
+// loginForm.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   const formData = new FormData(event.target);
+//   const email = formData.get('email');
+//   const password = formData.get('password');
+
+//   const response = await fetch('/users/login', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify({ email, password }),
+//   });
+
+	
+// 	const data = await response.json();
+// 	if (response.status === 401) {
+// 	  alert(data.error);
+// 	} else if (response.status === 200) {
+// 	  window.location.href = '/views/home.html';
+// 	}
+// });
+
+
 const loginForm = document.getElementById('login-form');
 
 loginForm.addEventListener('submit', async (event) => {
@@ -7,19 +34,29 @@ loginForm.addEventListener('submit', async (event) => {
   const email = formData.get('email');
   const password = formData.get('password');
 
-  const response = await fetch('/users/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ email, password }),
-  });
+  try {
+    const response = await fetch('/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-	
-	const data = await response.json();
-	if (response.status === 401) {
-	  alert(data.error);
-	} else if (response.status === 200) {
-	  window.location.href = '/views/home.html';
-	}
+    if (!response.ok) {
+      console.error(`Error: ${response.status}`);
+      return;
+    }
+
+    const data = await response.json();
+
+    if (response.status === 401) {
+      alert(data.error);
+    } else if (response.status === 200) {
+      window.location.href = '/views/home.html';
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
+
